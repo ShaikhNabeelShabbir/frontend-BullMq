61fb05ac88aff759dc4499c202f51a8658cb94e4
Nabeel <nabeelshaikh@Nabeels-MacBook-Pro.local>
2024-09-13 11:13:52 +0530

    working on telegra button

--- src/pages/telegram-login-button.tsx
+++ src/pages/telegram-login-button.tsx
@@ -3,12 +3,13 @@ import { LoginButton } from "@telegram-auth/react";
 const TelegramloginButton = () => {
   return (
     <LoginButton
-      botUsername={"Phenoxide_bot"}
+      botUsername={"tg_react_auth_test_bot"}
       buttonSize="large" // "large" | "medium" | "small"
       cornerRadius={5} // 0 - 20
+      showAvatar={true} // true | false
       lang="en"
       onAuthCallback={(authData) => {
-        console.log("hello", authData);
+        console.log(authData);
       }}
     />
   );
